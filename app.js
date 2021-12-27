f0b0b0692874e20f83a5bb1b7492925822b40167
PIYUSH VERMA <b20163@students.iitmandi.ac.in>
2021-12-27 21:49:46 +0530

    Starting code files added

---
+++ app.js
@@ -0,0 +1,85 @@
+require('dotenv').config();
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const express = require("express");
+const ejs = require("ejs")
+const mongoose = require("mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+const _ = require('lodash');
+require('https').globalAgent.options.rejectUnauthorized = false;
+
+///////   Dependency requirements above    ///////
+
+const app = express();
+app.use(express.static("public"));
+app.use(express.urlencoded({extended : true}));
+app.set('view engine', 'ejs')
+
+app.use(session({
+    secret : process.env.SECRET,
+    resave : false,
+    saveUninitialized : false
+}));
+
+app.use(passport.initialize()); 
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/Ruvaan", {useNewUrlParser: true,useUnifiedTopology: true,});
+// mongoose.connect(String(process.env.PASS),{ useNewUrlParser: true , useUnifiedTopology: true});
+
+
+// Create DB schema here
+
+// userSchema.plugin(passportLocalMongoose,{
+//     usernameField : "username"
+// });
+// userSchema.plugin(findOrCreate);
+
+// const User = mongoose.model("user", userSchema);
+
+// passport.use(User.createStrategy())
+
+////////  Creating sessions and serializing   //////////
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+////////Google OAuth 2.0 Strategy/////////
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    // callbackURL: "https://iit-mandi-vaccination-database.herokuapp.com/auth/google/secrets",
+    callbackURL: "http://localhost:3000/auth/google/markos",
+    userProfileUrl : "https://www.googleapis.com.oauth2.v3.userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ username: profile.id},
+        {
+            name : profile._json.name,
+            pic : profile._json.picture,
+            email: profile._json.email
+        }, 
+        function (err, user) {
+        console.log(profile.displayName);
+      return cb(err, user);
+    });
+  }
+));
+
+// Home route
+app.get('/', function(req,res){
+    res.send("Ola");
+    console.log("Ruvan 2022");
+})
+
+app.listen(3000||process.env.PORT, function(){
+    console.log("Server running on port 3000");
+})
