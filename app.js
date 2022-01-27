bc4f5a0c2cb973a2b50393e791a863f639f6e56b
Vinayak Sachan <vinayakasachan@gmail.com>
2022-01-28 00:44:53 +0530

    Custom Scrollbar added

--- app.js
+++ app.js
@@ -77,56 +77,56 @@ app.set("view engine", "ejs");
 // ));
 
 // ROUTES
-app.get("/", function (req, res) {
-  res.render("home", {});
+app.get("/", function(req, res) {
+    res.render("home", {});
 });
-app.get("/about", function (req, res) {
-  res.render("about", {});
+app.get("/about", function(req, res) {
+    res.render("about", {});
 });
-app.get("/contact", function (req, res) {
-  res.render("contact", {});
+app.get("/contact", function(req, res) {
+    res.render("contact", {});
 });
-app.get("/events", function (req, res) {
-  res.render("events", {});
+app.get("/events", function(req, res) {
+    res.render("events", {});
 });
 
 // Subscription or Contact form submission
 app.post("/subscribe", (req, res) => {
-  console.log(req.body);
+    console.log(req.body);
 
-  // SMTP Server
-  async function main() {
-    const subscriber = req.body.email;
+    // SMTP Server
+    async function main() {
+        const subscriber = req.body.email;
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: "smtp-mail.outlook.com",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: "piyushverma0007@outlook.com", // generated ethereal user
-        pass: process.env.mailPASS, // generated ethereal password
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    });
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: "smtp-mail.outlook.com",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: "piyushverma0007@outlook.com", // generated ethereal user
+                pass: process.env.mailPASS, // generated ethereal password
+            },
+            tls: {
+                rejectUnauthorized: false,
+            },
+        });
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: '"Kongthong" <piyushverma0007@outlook.com>', // sender address
-      to: subscriber, // list of receivers
-      subject: "Subscription", // Subject line
-      text: "Hello world", // plain text body
-      html: "You are subscribed", // html body
-    });
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: '"Kongthong" <piyushverma0007@outlook.com>', // sender address
+            to: subscriber, // list of receivers
+            subject: "Subscription", // Subject line
+            text: "Hello world", // plain text body
+            html: "You are subscribed", // html body
+        });
 
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  }
-  main().catch(console.error);
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    }
+    main().catch(console.error);
 });
 
-app.listen(3000 || process.env.PORT, function () {
-  console.log("Server running on port 3000");
+app.listen(3000 || process.env.PORT, function() {
+    console.log("Server running on port 3000");
 });
