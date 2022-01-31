fa20d72401a637e4028da196e658bed0e89a1358
Vinayak Sachan <vinayakasachan@gmail.com>
2022-01-31 17:29:27 +0530

    Minor changes pre-deploy

--- public/js/faq.js
+++ public/js/faq.js
@@ -1,20 +1,20 @@
 const Ques = [
-  "Are the events paid?",
-  "What is the last date to register?",
-  "Can non IIT sudents also participate?",
-  "Do we have to come to IIT Mandi to participate?",
-  "Are all the events completely online?",
+    "Are the events paid?",
+    "What is the last date to register?",
+    "Can non IIT sudents also participate?",
+    "Do we have to come to IIT Mandi to participate?",
+    "Are all the events completely online?",
 ];
 const Ans = [
-  "No. Some events are paid while others are open for everyone to participate without any entry barrier.",
-  "For every event you must register at least 24 hours prior to the commencement.",
-  "Ruvaan is open for everyone. You need not be a college student all together to participate.",
-  "No. Since the events are in online mode, your presence is neigther required nor expected. Bhenchod hum toh pohche nahi hain tumko pehe jana hai.",
-  "While all events are completely online for attendees there might be performances which will be aired directly from IIT Mandi",
+    "No. Some events are paid while others are open for everyone to participate without any entry barrier.",
+    "For every event you must register at least 24 hours prior to the commencement.",
+    "Ruvaan is open for everyone. You need not be a college student all together to participate.",
+    "No. Since the events are in online mode, your presence is neigther required nor expected.Hum toh pohche nahi hain tumko pehe jana hai.",
+    "While all events are completely online for attendees there might be performances which will be aired directly from IIT Mandi",
 ];
 
 function addtoAccordion(que, ans, i) {
-  let faqAccordian = `<div class="accordion-item">
+    let faqAccordian = `<div class="accordion-item">
     <h2 class="accordion-header" id="flush-heading${i}">
         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${i}" aria-expanded="false" aria-controls="flush-collapse${i}">
         ${que}
@@ -26,26 +26,27 @@ function addtoAccordion(que, ans, i) {
         </div>
     </div>
     </div>`;
-  $(".accordion").append(faqAccordian);
+    $(".accordion").append(faqAccordian);
 }
 let pastState = "";
-function faqValue() {
-  let faqSearch = $(".search-box").val();
-  if (pastState !== faqSearch) {
-    pastState = faqSearch;
-    $(".accordion-item").remove();
-    $(".collapse").remove();
-    if (faqSearch.length !== 0) {
-      console.log(faqSearch);
-      let pattern = new RegExp(faqSearch, "i");
 
-      Ques.forEach(function (Q, index) {
-        let result = Q.match(pattern);
-        if (result !== null) {
-          addtoAccordion(Ques[index], Ans[index], index);
+function faqValue() {
+    let faqSearch = $(".search-box").val();
+    if (pastState !== faqSearch) {
+        pastState = faqSearch;
+        $(".accordion-item").remove();
+        $(".collapse").remove();
+        if (faqSearch.length !== 0) {
+            console.log(faqSearch);
+            let pattern = new RegExp(faqSearch, "i");
+
+            Ques.forEach(function(Q, index) {
+                let result = Q.match(pattern);
+                if (result !== null) {
+                    addtoAccordion(Ques[index], Ans[index], index);
+                }
+            });
         }
-      });
     }
-  }
 }
 setInterval(faqValue, 1000);
