51e7719b4bde5ceefa9c0b7b97753095682853eb
Rishabh Maheshwari <79650357+Rishabh-792@users.noreply.github.com>
2022-03-01 16:07:19 +0530

    simple login

--- app.js
+++ app.js
@@ -8,10 +8,26 @@ const ejsMate = require("ejs-mate");
 const mongoose = require("mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const flash = require("connect-flash");
 // const _ = require('lodash');
 require("https").globalAgent.options.rejectUnauthorized = false;
 
 ///////   Dependency requirements above    ///////
+
+// Connecting to local datatabse
+const dbName = "Ruvaan"
+mongoose.connect(`mongodb://localhost:27017/${dbName}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 const app = express();
 app.use(express.static("public"));
@@ -21,15 +37,40 @@ app.use(express.urlencoded({ extended: true }));
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 
-app.use('/robots.txt', function(req, res, next) {
-    res.type('text/plain')
+app.use("/robots.txt", function (req, res, next) {
+    res.type("text/plain");
     res.send("User-agent: *\nDisallow: /team_images");
 });
-// app.use(session({
-//     secret : process.env.SECRET,
-//     resave : false,
-//     saveUninitialized : false
-// }));
+
+const sessionConfig = {
+    name: "Ruvaan",
+    secret: "thisshouldbeabettersecret!",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+};
+
+app.use(session(sessionConfig));
+
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+});
 
 // app.use(passport.initialize());
 // app.use(passport.session());
@@ -81,34 +122,66 @@ app.use('/robots.txt', function(req, res, next) {
 // ));
 
 // ROUTES
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     // res.render("home", {});
-    res.redirect('/events');
+    res.redirect("/events");
 });
-app.get("/about", function(req, res) {
+app.get("/about", function (req, res) {
     res.render("about", {});
 });
-app.get("/contact", function(req, res) {
+app.get("/contact", function (req, res) {
     res.render("contact", {});
 });
-app.get("/events", function(req, res) {
+app.get("/events", function (req, res) {
     res.render("events", {});
 });
-app.get("/sponsors", function(req, res) {
+app.get("/sponsors", function (req, res) {
     res.render("sponsors", {});
 });
-app.get("/faq", function(req, res) {
+app.get("/faq", function (req, res) {
     res.render("faq", {});
 });
-app.get("/team", function(req, res) {
+app.get("/team", function (req, res) {
     res.render("team", {});
 });
-app.get("/workshop", function(req, res) {
+app.get("/workshop", function (req, res) {
     res.render("workshop", {});
 });
-app.get("/merchandise", function(req, res) {
+app.get("/merchandise", function (req, res) {
     res.render("merchandise", {});
 });
+app.get("/register", (req, res) => {
+    res.render("users/register");
+});
+app.post("/register", async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            req.flash('success', 'Welcome!');
+            res.redirect('/events');
+        })
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+});
+app.get("/login", (req, res) => {
+    res.render("users/login");
+});
+app.post(
+    "/login",
+    passport.authenticate("local", {
+        failureFlash: true,
+        failureRedirect: "/login",
+    }),
+    (req, res) => {
+        req.flash("success", "Welcome back!");
+        res.render('events');
+    }
+);
+
 // Subscription or Contact form submission
 app.post("/subscribe", (req, res) => {
     console.log(req.body);
@@ -146,6 +219,6 @@ app.post("/subscribe", (req, res) => {
     main().catch(console.error);
 });
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server running on port 3000");
 });
